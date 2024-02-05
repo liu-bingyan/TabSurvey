b57269dfeefcf3f55ae13dc3d44ae580ec0ed924
Bingyan  Liu <bingyan2@illinois.edu>
2024-02-05 15:09:55 -0600

    Update testall.sh

--- testall.sh
+++ testall.sh
@@ -38,8 +38,8 @@ MODELS=( ####["LinearModel"]=$SKLEARN_ENV
          ####["DANet"]=$TORCH_ENV
           )
 
-CONFIGS=( "config/adult.yml"
-          #"config/covertype.yml"
+CONFIGS=( #"config/adult.yml"
+          "config/covertype.yml"
           #"config/california_housing.yml"
           #"config/higgs.yml"
           )
