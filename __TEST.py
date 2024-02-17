f0a53c79658494f4871210b6a1684a0b0d26833b
Bingyan  Liu <bingyan2@illinois.edu>
2024-02-16 20:24:15 -0600

    1

--- __TEST.py
+++ __TEST.py
@@ -1,9 +1,12 @@
-@profile    
-def func():
-  for i in range(1000):
-    print(i)
-  print("done")
-  
-if __name__ == '__main__':
-  func()
+import torch
+import numpy as np
 
+@profile
+def shutest():
+  x = torch.rand(1000000, 1000)
+  batch_x = x[0:1000]
+  indices = torch.randperm(x.size(0))
+  shuffled_x = x[indices]
+
+if __name__ == "__main__": 
+  shutest()
--- __gflops2.py
+++ __gflops2.py
@@ -29,17 +29,20 @@ class MLP(nn.Module):
 
 @profile
 def run(args):
+    print(args)
     input_size = 54
     hidden_dim = 99
     num_samples = 581012
     num_epochs = 300
-    learning_rate = 7.0e-4
+    learning_rate = math.sqrt(args.batch_size/num_samples)*0.1
 
     # Create an instance of the LinearModel
-   # model = LinearModel(input_size, hidden_dim)
+    # model = LinearModel(input_size, hidden_dim)
     
     model = MLP(input_size, hidden_dim)
-
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
     num_connections  = model.connections
     GFLO = num_epochs*6*num_samples*num_connections / 1e9
     print(f"GFLO is {GFLO:.2f}")
@@ -51,61 +54,80 @@ def run(args):
     y = y-1
     y = torch.tensor(y, dtype=torch.long)
     y = F.one_hot(y, num_classes=7).to(torch.float32)
+
     # Move the data and model to GPU if available
+    print('cuda is available:', torch.cuda.is_available())
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #print('x is on device:', x.device)
+ 
     x = x.to(device)
-    #print('x is on device:', x.device)
-
-    #print('y is on device:', y.device)
     y = y.to(device)
-    #print('y is on device:', y.device)
 
+
+
+    
     model = model.to(device)
-    print(f'model is on cuda: Don`t know, x is on device: {x.device}, y is on device: {y.device}')
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = criterion.to(device)
+    #optimizer = optimizer.to(device)
     
     total_timer = timer.Timer()
     epoch_timer = timer.Timer()
+
     # Train the model
     total_timer.start()
     
-    print('construct dataset')
     # Create a TensorDataset
     dataset = TensorDataset(x, y)
 
-    print('construct dataloader')
     # Create a DataLoader with batch size
     batch_size = args.batch_size
     print('batch_size:', batch_size)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=args.shuffle)#, num_workers=args.num_workers, pin_memory=args.pin_memory, drop_last=args.drop_last)
-    print('dataloader is on device : don`t know')
 
-    torch.cuda.synchronize()
+
+    #torch.cuda.synchronize()
     print('start training the model')
     for epoch in range(num_epochs):
         epoch_timer.start()
         
-        for i, (batch_x, batch_y) in enumerate(dataloader):
-          
-            # Forward pass
-            outputs = model(batch_x)
-            loss = criterion(outputs, batch_y)
+        if args.data_loader:
+            for i, (batch_x, batch_y) in enumerate(dataloader):
+            
+                # Forward pass
+                outputs = model(batch_x)
+                loss = criterion(outputs, batch_y)
+
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+        elif args.batching:
+            for i in range(0, num_samples, batch_size):
+                batch_x = x[i:i+batch_size]
+                batch_y = y[i:i+batch_size]
+                # Forward pass
+                outputs = model(batch_x)
+                loss = criterion(outputs, batch_y)
+
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            outputs = model(x)
+            loss = criterion(outputs, y)
 
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        epoch_timer.end()        
         # Print progress
-        if (epoch+1) % 1 == 0:
+        if (epoch<10)| (epoch % 10 == 0):
             print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+            print('avg time for epoch:', epoch_timer.get_average_time())
 
-        epoch_timer.end()
-        print('avg time for epoch:', epoch_timer.get_average_time())
-    torch.cuda.synchronize()
+    #torch.cuda.synchronize()
     total_timer.end()
 
     # Test the model
@@ -116,15 +138,16 @@ def run(args):
         print(f'Test Loss: {test_loss.item():.4f}')
 
     print(f'total_timer : {total_timer.get_average_time()}')
-    print(f'epoch_timer : {epoch_timer.get_average_time()}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Example script with named arguments")
     parser.add_argument("--batch_size", type=int, default=16384, help="Batch size for training")
-    parser.add_argument("--shuffle", type=bool, default=False, help="Shuffle the dataset")
+    parser.add_argument("--shuffle", type=bool, default=False, help="Shuffle the dataset",action=argparse.BooleanOptionalAction)
     parser.add_argument("--num_workers", type=int, default=4, help="Number of workers for dataloader")  
-    parser.add_argument("--pin_memory", type=bool, default=False, help="Pin memory for faster transfer to GPU")
-    parser.add_argument("--drop_last", type=bool, default=False, help="Drop the last batch if it is not complete")
+    parser.add_argument("--pin_memory", type=bool, default=False, help="Pin memory for faster transfer to GPU", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--drop_last", type=bool, default=False, help="Drop the last batch if it is not complete", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--data_loader", type=bool, default=False, help="Use dataloader or not", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--batching", type=bool, default=False, help="Use dataloader or not", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
     run(args)
