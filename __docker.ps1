86eb6a85c78b1214db948aeed6c03c3475999221
Bingyan Liu <78130918+liu-bingyan@users.noreply.github.com>
2024-02-25 15:35:45 -0600

    Delete __docker.ps1

--- __docker.ps1
+++
@@ -1,7 +0,0 @@
-#docker build -t tab-survey .
-#docker run -v ~/output:/opt/notebooks/output -p 3123:3123 --rm -it tab-survey
-#docker run -v ~/output:/opt/notebooks/output -p 3123:3123 --rm -it --gpus '"device=0"' tab-survey
-docker run --shm-size=16G -v ~/output:/opt/notebooks/output -p 3123:3123 --rm -it --gpus all tab-survey
-# opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=3123 --no-browser --allow-root
-
-
